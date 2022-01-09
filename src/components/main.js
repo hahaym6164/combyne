1db0a58f205dd917b1d44badd25d06f580a1dea8
hahaym6164 <60290070+hahaym6164@users.noreply.github.com>
2022-01-09 00:24:51 -0500

    update

--- src/components/main.js
+++ src/components/main.js
@@ -9,7 +9,8 @@ export default class Main extends Component{
            grid:  generateRandomGrid(20,20),
            colors: COLORS,
            X :0,
-           Y:0
+           Y:0,
+           inputFormatUsable: true,
        }
        
     }
@@ -19,9 +20,17 @@ export default class Main extends Component{
         let y = e.target[2].value
         let color = e.target[0].value
         let grid = this.state.grid
-       this.setState({
-           grid:floodFillAt(grid,x,y,color)
-       }) 
+        if(y>=0 && y<=19 && x>=0 && x<=19 ){
+            this.setState({
+                inputFormatUsable :true, 
+                grid:floodFillAt(grid,x,y,color)
+            }) 
+        } else {
+            this.setState({
+                inputFormatUsable :false 
+            })
+        }
+       
     }
     pixelOnClick(e){
         let coor = e.target.id.split(',')
@@ -45,7 +54,7 @@ export default class Main extends Component{
         console.log(value,e.target.id);
     }
     render(){
-        let {grid,colors} = this.state
+        let {grid,colors,inputFormatUsable} = this.state
   return (
       <div className="main">
           <form  className="function-form" onSubmit={this.btnClick.bind(this)}>
@@ -57,14 +66,28 @@ export default class Main extends Component{
    ]
   </select>
   <br/>
-  <p>You can input the coordinate <br/>below or click on the paint board</p>
+  <p>You can input the coordinate <br/>below or click on the paint board</p> 
+  {inputFormatUsable ? '':<p style={{color:'red'}}> ERROR! The inputs should be numbers<br/> between and including 0 and 19 </p>}
   X:<input id="X" onChange={this.inputChange.bind(this)} value={this.state.X}/> <br/>
  Y:<input id="Y" onChange={this.inputChange.bind(this)} value={this.state.Y}/> <br/>
     <input type="submit" value="Submit"/>
 </form>
     <div className="grid">
+        <div className="x-coor">         X ----->
+</div>
+        <div className="y-coor">
+
+            Y<br/>
+            |<br></br>
+            |<br></br>
+            |<br></br>
+            |<br></br>
+            \/
+        </div>
      { grid && grid.map( (rows,rowsIndex) =>(
+    
           <div key={rowsIndex} className="pixel-rows"> 
+          
          { rows && rows.map((pixel,index) =>(
              <div className="pixel" onClick={this.pixelOnClick.bind(this)} key ={index} id={rowsIndex+','+index} style={{ background: pixel}}>
                 </div>
